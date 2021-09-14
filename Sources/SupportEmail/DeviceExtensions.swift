a1e14de7f511a035829cdcb16b2d5f57f2f35989
Stephen Hayes <schayes4@gmail.com>
2021-09-14 18:03:37 -0400

    Fixes #40 add iPhone 13 model identifiers

--- Sources/SupportEmail/DeviceExtensions.swift
+++ Sources/SupportEmail/DeviceExtensions.swift
@@ -96,6 +96,14 @@ extension UIDevice {
             return "iPhone 12 Pro"
         case "iPhone13,4":
             return "iPhone 12 Pro Max"
+        case "iPhone14,2":
+            return "iPhone 13 Pro"
+        case "iPhone14,3":
+            return "iPhone 13 Pro Max"
+        case "iPhone14,4":
+            return "iPhone 13 Mini"
+        case "iPhone14,5":
+            return "iPhone 13"
 
         // iPads
 
